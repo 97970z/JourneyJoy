49d2b171486af01cd7263923846b9c6863932096
97970z <97970zz@gmail.com>
2024-03-09 14:00:59 +0900

    장소 디테일 라우터

--- backend-journeyjoy/routes/placeRoutes.js
+++ backend-journeyjoy/routes/placeRoutes.js
@@ -44,6 +44,19 @@ router.get("/search", async (req, res) => {
   }
 });
 
+// 특정 장소 가져오기
+router.get("/:id", async (req, res) => {
+  try {
+    const place = await Place.findById(req.params.id);
+    if (!place) {
+      return res.status(404).json({ message: "Place not found" });
+    }
+    res.json(place);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // 모든 장소 가져오기
 router.get("/", async (req, res) => {
   try {
