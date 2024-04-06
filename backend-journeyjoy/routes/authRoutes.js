968d917fde0d9e67f36f36dc7ea766b073b35352
97970z <97970zz@gmail.com>
2024-04-06 22:45:00 +0900

    백엔드 리팩토링 [회원가입 Validation 적용]

--- backend-journeyjoy/routes/authRoutes.js
+++ backend-journeyjoy/routes/authRoutes.js
@@ -3,25 +3,44 @@ import { Router } from "express";
 import pkg from "bcryptjs";
 import jwt from "jsonwebtoken";
 import User from "../models/User.js";
+import { body, validationResult } from "express-validator";
+import { jwtSecret, refreshTokenSecret } from "../config/envConfig.js";
 
 const router = Router();
 const { hash, compare } = pkg;
 
 // 회원가입
-router.post("/register", async (req, res) => {
-  try {
-    console.log(req.body);
-    const hashedPassword = await hash(req.body.password, 10);
-    const newUser = new User({
-      username: req.body.username,
-      password: hashedPassword,
-    });
-    await newUser.save();
-    res.status(201).json({ message: "User created successfully." });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+router.post(
+  "/register",
+  [
+    body("username")
+      .isString()
+      .isLength({ min: 4 })
+      .withMessage("유저네임은 문자열이어야 하며 4자 이상이어야 합니다."),
+    body("password")
+      .isLength({ min: 4 })
+      .withMessage("비밀번호는 4자 이상이어야 합니다."),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      console.log(req.body);
+      const hashedPassword = await hash(req.body.password, 10);
+      const newUser = new User({
+        username: req.body.username,
+        password: hashedPassword,
+      });
+      await newUser.save();
+      res.status(201).json({ message: "User created successfully." });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   }
-});
+);
 
 // 로그인
 router.post("/login", async (req, res) => {
@@ -30,14 +49,12 @@ router.post("/login", async (req, res) => {
     if (user && (await compare(req.body.password, user.password))) {
       const accessToken = jwt.sign(
         { userId: user._id },
-        process.env.JWT_SECRET, // 환경변수에서 JWT 비밀키를 가져옵니다.
+        jwtSecret, // 환경변수에서 JWT 비밀키를 가져오기.
         { expiresIn: "1h" } // 토큰의 유효 기간을 1시간으로 설정
       );
-      const refreshToken = jwt.sign(
-        { userId: user._id },
-        process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: "7d" }
-      );
+      const refreshToken = jwt.sign({ userId: user._id }, refreshTokenSecret, {
+        expiresIn: "7d",
+      });
 
       user.refreshToken = refreshToken;
       await user.save();
@@ -59,18 +76,16 @@ router.post("/refresh", async (req, res) => {
     return res.status(401).json({ message: "Refresh Token is required" });
 
   try {
-    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    const decoded = jwt.verify(refreshToken, refreshTokenSecret);
     const user = await User.findById(decoded.userId);
     console.log("user:", user);
     if (user.refreshToken !== refreshToken) {
       return res.status(403).json({ message: "Refresh Token is not valid" });
     }
 
-    const newAccessToken = jwt.sign(
-      { userId: user._id },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
+    const newAccessToken = jwt.sign({ userId: user._id }, jwtSecret, {
+      expiresIn: "1h",
+    });
     console.log("newAccessToken:", newAccessToken);
     res.json({ accessToken: newAccessToken });
   } catch (error) {
