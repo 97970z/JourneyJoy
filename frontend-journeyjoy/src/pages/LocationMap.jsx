8a1db9d2091d1cd6b708d643bcc5da3b55492805
97970z <97970zz@gmail.com>
2024-04-15 14:27:04 +0900

    장소 검색 기능 추가

--- frontend-journeyjoy/src/pages/LocationMap.jsx
+++ frontend-journeyjoy/src/pages/LocationMap.jsx
@@ -1,6 +1,14 @@
-// frontend/src/pages/LocationMap.jsx
+// // frontend/src/pages/LocationMap.jsx
 import { useState, useEffect } from "react";
-import { Box, IconButton, Typography } from "@mui/material";
+import {
+	Box,
+	IconButton,
+	Typography,
+	TextField,
+	Button,
+	Snackbar,
+	Alert,
+} from "@mui/material";
 import TuneSharpIcon from "@mui/icons-material/TuneSharp";
 import { usePlaces } from "../contextAPI/PlacesContext";
 import PlacesFilter from "../components/LocationMap/PlacesFilter";
@@ -8,8 +16,10 @@ import PlacesMap from "../components/LocationMap/PlacesMap";
 
 function LocationMap() {
 	const { apiPlaces, fetchExternalPlaces } = usePlaces();
-	const [selectedSido, setSelectedSido] = useState("");
 	const [sidos, setSidos] = useState([]);
+	const [selectedSido, setSelectedSido] = useState("");
+	const [searchTerm, setSearchTerm] = useState("");
+	const [openAlert, setOpenAlert] = useState(false);
 	const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
 	useEffect(() => {
@@ -22,14 +32,12 @@ function LocationMap() {
 			].sort();
 			setSidos(["All", ...uniqueSidos]);
 		};
-
 		fetchData();
 	}, [apiPlaces, fetchExternalPlaces]);
 
-	const filteredPlaces =
-		selectedSido !== "All"
-			? apiPlaces.filter((place) => place.sido === selectedSido)
-			: apiPlaces;
+	const filteredPlaces = apiPlaces.filter((place) => {
+		return selectedSido === "All" || place.sido === selectedSido;
+	});
 
 	const toggleDrawer = (open) => (event) => {
 		if (
@@ -41,6 +49,23 @@ function LocationMap() {
 		setIsDrawerOpen(open);
 	};
 
+	const handleSearch = () => {
+		if (selectedSido !== "All") {
+			handleOpenAlert();
+		}
+	};
+
+	const handleOpenAlert = () => {
+		setOpenAlert(true);
+	};
+
+	const handleCloseAlert = (event, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setOpenAlert(false);
+	};
+
 	return (
 		<Box sx={{ height: "100vh", width: "100%" }}>
 			<Box
@@ -48,6 +73,7 @@ function LocationMap() {
 					display: "flex",
 					justifyContent: "center",
 					alignItems: "center",
+					flexDirection: "column",
 					margin: "10px 0 10px 0",
 				}}
 			>
@@ -55,7 +81,37 @@ function LocationMap() {
 					<TuneSharpIcon fontSize="large" />
 					<Typography variant="h6">Filter</Typography>
 				</IconButton>
+				<Box style={{ display: "flex", alignItems: "center", width: "80%" }}>
+					<TextField
+						label="Search by Movie Title"
+						variant="outlined"
+						value={searchTerm}
+						onChange={(e) => setSearchTerm(e.target.value)}
+						style={{ flex: 1 }}
+					/>
+					<Button
+						onClick={handleSearch}
+						variant="contained"
+						color="primary"
+						style={{ marginLeft: "10px" }}
+					>
+						Search
+					</Button>
+				</Box>
 			</Box>
+			<Snackbar
+				open={openAlert}
+				autoHideDuration={6000}
+				onClose={handleCloseAlert}
+			>
+				<Alert
+					onClose={handleCloseAlert}
+					severity="warning"
+					sx={{ width: "100%" }}
+				>
+					필터에서 All을 선택하시고 검색해주세요!
+				</Alert>
+			</Snackbar>
 			<PlacesFilter
 				isOpen={isDrawerOpen}
 				toggleDrawer={toggleDrawer}
