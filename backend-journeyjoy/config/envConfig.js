58952118b945cfaed7c878dc26dc728cbf42e7e9
97970z <97970zz@gmail.com>
2024-04-25 20:21:32 +0900

    env PORT 설정

--- backend-journeyjoy/config/envConfig.js
+++ backend-journeyjoy/config/envConfig.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-export const PORT = process.env.PORT || 5000;
+export const PORT = process.env.PORT;
 export const mongoURI = process.env.MONGODB_URI;
 export const jwtSecret = process.env.JWT_SECRET;
 export const refreshTokenSecret = process.env.REFRESH_TOKEN_SECRET;
