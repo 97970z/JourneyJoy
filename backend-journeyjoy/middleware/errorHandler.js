041fe24098e9487c7a17dce16ae1557487e0b6ab
97970z <97970zz@gmail.com>
2024-05-14 13:02:42 +0900

    중복 에러 코드 처리

--- backend-journeyjoy/middleware/errorHandler.js
+++ backend-journeyjoy/middleware/errorHandler.js
@@ -1,6 +1,18 @@
 // backend/middleware/errorHandler.js
 const errorHandler = (err, req, res, next) => {
   console.error(err);
+  if (err.name === "ValidationError") {
+    const messages = Object.values(err.errors).map((val) => val.message);
+    return res.status(400).json({ errors: messages });
+  }
+
+  if (err.code && err.code === 11000) {
+    const field = Object.keys(err.keyValue)[0];
+    const value = err.keyValue[field];
+    const message = `이미 사용 중인 ${field} '${value}'입니다. 다른 ${field}을 입력하세요.`;
+    return res.status(400).json({ message });
+  }
+
   res.status(500).json({ message: err.message });
 };
 
